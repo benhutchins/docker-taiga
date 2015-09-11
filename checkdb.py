142dc1776112fb1911da3564d9a2097e8a1f82e6
Benjamin Hutchins <ben@hutchins.co>
2015-09-11 15:42:33 -0400

    Correcting variable name (fixes #3).

--- checkdb.py
+++ checkdb.py
@@ -5,7 +5,7 @@ DB_HOST = os.getenv('POSTGRES_PORT_5432_TCP_ADDR') or os.getenv('TAIGA_DB_HOST')
 DB_USER = os.getenv('TAIGA_DB_USER')
 DB_PASS = os.getenv('POSTGRES_ENV_POSTGRES_PASSWORD') or os.getenv('TAIGA_DB_PASSWORD')
 
-conn = psycopg2.connect("dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_USER + "' password='" + DB_PASS + "'")
+conn = psycopg2.connect("dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_HOST + "' password='" + DB_PASS + "'")
 cur = conn.cursor()
 
 cur.execute("select * from information_schema.tables where table_name=%s", ('django_migrations',))
