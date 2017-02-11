30f1c6040dc7bb698c123f46e3b26ef9432771ba
Benjamin Hutchins <ben@hutchins.co>
2017-02-10 21:44:02 -0500

    Echo out database connection
    This is very helpful for debugging a database connection, found this in some work by @jstty

--- checkdb.py
+++ checkdb.py
@@ -5,12 +5,14 @@ DB_HOST = os.getenv('TAIGA_DB_HOST')
 DB_USER = os.getenv('TAIGA_DB_USER')
 DB_PASS = os.getenv('POSTGRES_ENV_POSTGRES_PASSWORD') or os.getenv('TAIGA_DB_PASSWORD')
 
-conn = psycopg2.connect("dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_HOST + "' password='" + DB_PASS + "'")
+conn_string = "dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_HOST + "' password='" + DB_PASS + "'"
+print("Connecting to database:\n" + conn_string)
+conn = psycopg2.connect(conn_string)
 cur = conn.cursor()
 
 cur.execute("select * from information_schema.tables where table_name=%s", ('django_migrations',))
 exists = bool(cur.rowcount)
 
 if exists is False:
-    print('Database does not appear to be setup.')
+    print("Database does not appear to be setup.")
     sys.exit(2)
