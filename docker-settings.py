08a130cba666bd0e175c6fe527c253cf50f252e4
Andrey Yankin <yankin013@gmail.com>
2017-04-17 23:36:22 +0300

    Rename env variable TAIGA_EMAIL_ADDR to TAIGA_EMAIL_FROM to match Configuring SMTP section of README.md
    or it could be other way around to keep compatibility.

--- docker-settings.py
+++ docker-settings.py
@@ -40,7 +40,7 @@ if os.getenv('RABBIT_PORT') is not None and os.getenv('REDIS_PORT') is not None:
     EVENTS_PUSH_BACKEND_OPTIONS = {"url": "amqp://guest:guest@rabbit:5672//"}
 
 if os.getenv('TAIGA_ENABLE_EMAIL').lower() == 'true':
-    DEFAULT_FROM_EMAIL = os.getenv('TAIGA_EMAIL_ADDR')
+    DEFAULT_FROM_EMAIL = os.getenv('TAIGA_EMAIL_FROM')
     CHANGE_NOTIFICATIONS_MIN_INTERVAL = 300 # in seconds
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
